50c9b19be9c5eab7184debe96d06a5f83640f9ca
DuyDat13012003 <duydat13012003@gmail.com>
2026-02-27 15:08:41 +0700

    feat:bars tren navbar khi man hinh nho hon768 px va 992px

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ mode.addEventListener('click', function(){
         icon.classList.add('fa-moon');
     }
 })
+
+const bars = document.querySelector('.toggle-btn');
+const navbar = document.querySelector('header .navbar');
+
+    bars.addEventListener('click', function(){
+    if(bars.classList.contains('fa-bars')){
+        bars.classList.remove('fa-bars');
+        bars.classList.add('fa-xmark');
+        navbar.classList.add('slide');
+    } else {
+        bars.classList.remove('fa-xmark');
+        bars.classList.add('fa-bars');
+        navbar.classList.remove('slide');
+    }
+});
